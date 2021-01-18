dc62d7e691f843e5ba1893385d5c63a4ebc7632a
Cruz López Pérez <mcruzlp@gmail.com>
2021-01-18 02:13:41 +0100

    act

--- javascripts/funciones.js
+++ javascripts/funciones.js
@@ -37,12 +37,10 @@ $(document).ready(function(){
   });
 
 //SLIDESHOW ****************************
-var slideIndex = [1,4];
+var slideIndex = [1,1];
 var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
 showSlides(1, 0);
 showSlides(1, 1);
-showSlides(1, 2);
-showSlides(1, 3);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
@@ -51,10 +49,15 @@ function plusSlides(n, no) {
 function showSlides(n, no) {
   var i;
   var x = document.getElementsByClassName(slideId[no]);
+  var dots = document.getElementsByClassName("dot");
   if (n > x.length) {slideIndex[no] = 1}    
   if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
-  x[slideIndex[no]-1].style.display = "block";  
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  x[slideIndex[no]-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
